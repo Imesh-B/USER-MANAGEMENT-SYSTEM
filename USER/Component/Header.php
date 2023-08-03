929199108f057009a0d8f3c6ff396fee1dfc9b73
Imesh Bandara <imesh.fsd.info@gmail.com>
2023-08-03 23:47:27 +0530

    Create Header with Inline css

---
+++ USER/Component/Header.php
@@ -0,0 +1,36 @@
+<!--Header css section-->
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover {
+        background: #04AA6D;
+    }
+</style>
+<!-- Header section start-->
+<nav>
+    <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="Component/Form-Login.php">LOGIN</a></li>
+    </ul>
+</nav>
+<!-- header end -->
